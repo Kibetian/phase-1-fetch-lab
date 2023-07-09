7a030c7fe47a9540890ff6a0f4cc4c2b6cf79dc1
Your Name <iankibet87@gmail.com>
2023-07-09 21:40:03 +0800

    fetch lab

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.log('Error:', error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const bookList = document.querySelector('main');
+  
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const listItem = document.createElement('li');
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetchBooks();
+
+
